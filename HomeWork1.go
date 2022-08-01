bc9ab74ad76c2b88940de703867bdb59bad6c34d
Hüsamettin ARABACI <harabaci88@gmail.com>
2022-08-01 21:17:17 +0300

    Added Comments
    TODO ile tanımlanan yerler incelenmeli

--- HomeWork1.go
+++ HomeWork1.go
@@ -4,10 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println(add(15, 4))
+	//TODO
+	// projede değişken tanımlaması yok 
+	// fonksiyon kullanımı çok yetersiz belirli bir senaryo üzerinde çeşitlendirerek
+	// fonksiyonların farklı tanımlama biçimlerinide burada kullanalım
 }
 
 func add(x int, y int) int {
+	//TODO
+	//fonksiyon ismi ile yapılan işlem arasında tutarsızlık var
+	// fonksiyon ismi sadece toplama yapacağını söylüyor 
 	fmt.Println("İşlem Başarılı!")
+	
 	if x < 6 && x > 0 || y > 16 {
 		return x + y
 	} else if x > 10 || y < 5 {
