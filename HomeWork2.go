535e3b0afbd1dac838f7828e52ff0632363b77e6
Mansur Uyar <92828313+uyarmansur@users.noreply.github.com>
2022-08-02 17:21:58 +0300

    Update HomeWork2.go

--- HomeWork2.go
+++ HomeWork2.go
@@ -5,8 +5,9 @@ import "fmt"
 func main() {
 	pln := fmt.Println
 	playersIn := 15
-	playersOut := 7
-	rules(playersIn, playersOut)
+	playersOut := 8
+	totalPlayers := playersIn + playersOut
+	rules(totalPlayers, playersOut)
 	pln(rules)
 }
 
@@ -14,9 +15,11 @@ func rules(totalPlayers int, oneTeam int) {
 	pln := fmt.Println
 	if totalPlayers == 22 {
 		pln("The match can be played without any problem!")
-	} else if totalPlayers <= 22 && (oneTeam > 6 && oneTeam < 11) {
+	} else if totalPlayers <= 22 && oneTeam == 7 {
 		pln("If oneTeam lose any more player,they counted as a loser team!")
-	} else {
+	} else if oneTeam <= 6 {
 		pln("The match is over!")
+	} else {
+		pln("The match is can not be played!")
 	}
 }
